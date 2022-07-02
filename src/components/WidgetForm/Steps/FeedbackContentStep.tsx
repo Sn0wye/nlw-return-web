83c56b3eafcf11ee9c9e69b979253fea3ef825d6
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-02 19:12:01 -0300

    Remove API call for demo purposes

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,10 +1,10 @@
-import { ArrowLeft, Camera } from "phosphor-react";
-import React, { FormEvent, useState } from "react";
-import { FeedbackType, feedbackTypes } from "..";
-import { api } from "../../../services/api";
-import CloseButton from "../../CloseButton";
-import Loading from "../../Loading";
-import ScreenshotButton from "../ScreenshotButton";
+import { ArrowLeft, Camera } from 'phosphor-react';
+import React, { FormEvent, useState } from 'react';
+import { FeedbackType, feedbackTypes } from '..';
+import { api } from '../../../services/api';
+import CloseButton from '../../CloseButton';
+import Loading from '../../Loading';
+import ScreenshotButton from '../ScreenshotButton';
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -20,7 +20,7 @@ export default function FeedbackContentStep({
   const [screenshot, setScreenshot] = useState<string | null>(null);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState('');
   const [isSubmittingFeedback, setIsSubmittingFeedback] = useState(false);
 
   async function handleSubmitFeedback(e: FormEvent) {
@@ -28,11 +28,11 @@ export default function FeedbackContentStep({
 
     setIsSubmittingFeedback(true);
 
-    await api.post("/feedbacks", {
-      type: feedbackType,
-      comment,
-      screenshot,
-    });
+    // await api.post('/feedbacks', {
+    //   type: feedbackType,
+    //   comment,
+    //   // screenshot,
+    // });
 
     setIsSubmittingFeedback(false);
 
@@ -43,18 +43,18 @@ export default function FeedbackContentStep({
     <>
       <header>
         <button
-          type="button"
-          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          type='button'
+          className='top-5 left-5 absolute text-zinc-400 hover:text-zinc-100'
           onClick={onFeedbackRestartRequested}
         >
-          <ArrowLeft weight="bold" className="w-4 h-4" />
+          <ArrowLeft weight='bold' className='w-4 h-4' />
         </button>
 
-        <span className="text-xl leading-6 flex items-center gap-2">
+        <span className='text-xl leading-6 flex items-center gap-2'>
           <img
             src={feedbackTypeInfo.image.source}
             alt={feedbackTypeInfo.image.alt}
-            className="w-6 h-6"
+            className='w-6 h-6'
           />
           {feedbackTypeInfo.title}
         </span>
@@ -62,25 +62,25 @@ export default function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form onSubmit={(e) => handleSubmitFeedback(e)} className="my-4 w-full">
+      <form onSubmit={(e) => handleSubmitFeedback(e)} className='my-4 w-full'>
         <textarea
-          className="min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="Conte com detalhes o que está acontecendo"
+          className='min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
+          placeholder='Conte com detalhes o que está acontecendo'
           onChange={(e) => setComment(e.target.value)}
         />
 
-        <footer className="flex gap-2 mt-2">
+        <footer className='flex gap-2 mt-2'>
           <ScreenshotButton
             screenshot={screenshot}
             onScreenshotTook={setScreenshot}
           />
 
           <button
-            type="submit"
+            type='submit'
             disabled={comment.length === 0 || isSubmittingFeedback}
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+            className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500'
           >
-            {isSubmittingFeedback ? <Loading /> : "Enviar Feedback"}
+            {isSubmittingFeedback ? <Loading /> : 'Enviar Feedback'}
           </button>
         </footer>
       </form>
